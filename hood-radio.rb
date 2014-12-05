8cd2a8040e482f086f06cb5071dc0c2d5be9fb95
Spencer Smith <robertspencersmith@gmail.com>
2014-12-05 16:13:23 -0500

    Created classes for everything and generally refactored. Much cleaner to look at

--- hood-radio.rb
+++ hood-radio.rb
@@ -2,42 +2,68 @@ require 'open-uri'
 require 'json'
 require 'io/console'
 
-def get_urls_from_sub(subreddit)
+class Redditor
+  attr_reader :subreddits, :urls, :audio_sources
 
-  json_data = open("http://www.reddit.com/r/#{subreddit}/.json?limit=100")
-  parsed = JSON.parse(json_data.read)
+  def initialize(subreddits,audio_sources)
+    @subreddits = subreddits
+    @urls = []
+    @audio_sources = audio_sources    
+  end
 
-  urls = []
-  parsed["data"]["children"].each do |child|
-    child_url = {:title => child["data"]["title"], :url => child["data"]["url"]}
-    if child_url[:url].include? "soundcloud" or child_url[:url].include? "youtube"
-      urls << child_url
+  def get_urls_from_sub() 
+    @subreddits.each do |subreddit|
+      json_data = open("http://www.reddit.com/r/#{subreddit}/.json?limit=100")
+      parsed = JSON.parse(json_data.read)
+      parsed["data"]["children"].each do |child|
+        child_url = {:title => child["data"]["title"], :url => child["data"]["url"]}
+        @audio_sources.any? do |audio_source| 
+          if child_url[:url].include? audio_source
+            @urls << child_url
+          end
+        end
+      end
     end
+    prepare_playlist()
   end
-  return urls
-end
 
-def process_alive(pid)
-  begin
-    Process.kill(0,pid)
-    true
-  rescue Errno::ESRCH
-    false
+  def prepare_playlist()
+    @urls.uniq!
+    @urls.shuffle!
   end
 end
 
-hood_urls = get_urls_from_sub("hiphopheads")
-hood_urls << get_urls_from_sub("trap")
-hood_urls.shuffle!
+class Jukebox
+  attr_reader :playlist, :cache_path, :pid, :track_index
 
-hood_urls.each do |url|
-  FileUtils.rm Dir[".cache/current_track.*"]
-  puts "Going to get #{url[:url]}"
-  `youtube-dl -x \"#{url[:url]}\" -q -o \".cache/current_track.%(ext)s\"`
+  def initialize(playlist,cache_path)
+    @playlist = playlist
+    @cache_path = cache_path
+    @track_index = 0
+  end
 
-  puts "Playing #{url[:title]}...\n\n\n"
-  parent_pid = fork {`mplayer -quiet .cache/current_track.*`}
-  child_pid = `pgrep -P #{parent_pid}`.to_i
-  while process_alive(child_pid)
+  def play()
+    puts "Found #{@playlist.length()} tracks. Let's jam!"
+    @playlist.each do |track|
+      @track_index += 1
+      clobber_cache()
+      
+      puts "Grabbing #{track[:url]}..."
+      `youtube-dl -x \"#{track[:url]}\" -q -o \"#{@cache_path}/current_track.%(ext)s\"`
+
+      puts "Download complete, now playing track \##{@track_index}: #{track[:title]}\n\n"
+      #@pid = fork {`mplayer -quiet .cache/current_track.*`}
+    end
+    @track_index = 0
+    clobber_cache()
+  end
+
+  def clobber_cache()
+    FileUtils.rm Dir["#{@cache_path}/current_track.*"]
   end
 end
+
+redditor = Redditor.new(["hiphopheads"],["youtube"])
+redditor.get_urls_from_sub()
+juke = Jukebox.new(redditor.urls,".cache")
+juke.play()
